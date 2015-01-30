e99670ae473042665010964617327702f20cdce3
Patrik Zakhar <123patokt@gmail.com>
2015-01-30 17:07:22 +0100

    Make it work with cyanogenmod

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/bq/vegetalte/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vegetalte
-PRODUCT_NAME := omni_vegetalte
+PRODUCT_NAME := cm_vegetalte
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris E5
