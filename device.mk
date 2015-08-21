c79b6645386abec20778064f8fac428227cfcb79
Patrik Zakhar <123patokt@gmail.com>
2015-08-21 14:51:17 +0200

    y550: fix dt.img errors

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dt.img:dt.img
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_NAME := y550
